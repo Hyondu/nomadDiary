61db96464e74cdd793900ab2302c0b3d17b1453f
Hyondu <147678410+Hyondu@users.noreply.github.com>
2023-11-29 22:27:07 +0900

    add Ads

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from "expo-splash-screen";
+import Realm from "realm";
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useCallback, useEffect, useState } from "react";
+import Navigator from "./navigator";
+import { DBContext } from "./context";
+
+SplashScreen.preventAutoHideAsync();
+
+// Define your object model
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const [realm, setRealm] = useState(null);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const connection = await Realm.open({
+          path: "nomadDiaryDB",
+          schema: [FeelingSchema],
+        });
+        setRealm(connection);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (ready) {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      await SplashScreen.hideAsync();
+    }
+  }, [ready]);
+
+  if (!ready) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DBContext.Provider value={realm}>
+      <NavigationContainer onReady={onLayoutRootView}>
+        <Navigator />
+      </NavigationContainer>
+    </DBContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
